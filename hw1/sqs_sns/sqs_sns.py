afb5269d1b5188fa470731b2b47f3d7279c65845
julius-song <41063036+julius-song@users.noreply.github.com>
2019-10-14 02:39:52 -0400

    Update sqs_sns.py

--- hw1/sqs_sns/sqs_sns.py
+++ hw1/sqs_sns/sqs_sns.py
@@ -3,69 +3,58 @@ import boto3
 
 def lambda_handler(event, context):
     # TODO implement
-    sqs = boto3.client(
-        'sqs',
-        aws_access_key_id='AKIA4DDHBCIR6VG2IC7Y',
-        aws_secret_access_key='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'
-        )
+    sqs = boto3.client('sqs')
     response = sqs.receive_message(
         QueueUrl='https://sqs.us-east-1.amazonaws.com/831292248611/dining_concierge',
         MaxNumberOfMessages=1
         )
     
-    order = json.loads(response.get('Messages')[0]['Body'])
-        
     try:
-        if event["httpMethod"] == "POST":
-            try:
-                cuisine = order["cuisine"]
-
-                # TODO: invoke elasiticsearch
-                boto3.setup_default_session(region_name='us-east-2')
-                client = boto3.client('lambda')
-                response = client.invoke(
-                    FunctionName='arn:aws:lambda:us-east-2:685653151206:function:LF_db',
-                    Payload=json.dumps({'categories':cuisine}).encode("utf-8")
-                )
-                
-                response_payload = json.loads(response["Payload"].read().decode("utf-8"))
-                
-                #print(response_payload)
-                
-                restaurant = response_payload['body']['Item']
-
-            except Exception as err:
-                print(err)
-                return {
-                    'statusCode': 405, 
-                    'body': "Invalid input: " + json.dumps(cuisine),
-                    'err': str(err)
-                }
-        
-        else:
-            # if method not listed above
-            return {
-                'statusCode': 400, 
-                'body': "Bad request."
-            }
-
+        receiptHandler = response['Messages'][0]['ReceiptHandle']
+        order = json.loads(response['Messages'][0]['Body'])
     except Exception as err:
         return {
-            'statusCode': 500, 
-            'body': "Internal error - dining_concierge_lf2.",
+            'statusCode': 400, 
+            'body': 'Bad request when retrieving messgaes from SQS',
             'err': str(err)
         }
         
-    boto3.setup_default_session(region_name='us-east-1')
-    sns = boto3.client(
-        'sns',
-        aws_access_key_id='AKIA4DDHBCIR36TEDGGA',
-        aws_secret_access_key='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'
+    response = sqs.delete_message(
+        QueueUrl='https://sqs.us-east-1.amazonaws.com/831292248611/dining_concierge',
+        ReceiptHandle=receiptHandler
         )
+    
+    cuisine = order['cuisine'].lower()
+    if cuisine not in ['chinese', 'american', 'mexican', 'italian', 'japanese']:
+        return {
+            'statusCode': 405,
+            'body': 'Invalid input: {}'.format(order['cuisine'])
+        }
 
-    text = """Hello! Here are my {} restaurant suggestions for {} people, 
-    for {} at {}: {}, located at {}. Enjoy your meal!""".format(
-        order['cuisine'], order['numberOfPeople'], order['diningDate'], 
+    # TODO: invoke elasiticsearch
+    boto3.setup_default_session(region_name='us-east-2')
+    client = boto3.client('lambda')
+    response = client.invoke(
+        FunctionName='arn:aws:lambda:us-east-2:685653151206:function:LF_db',
+        Payload=json.dumps({'cuisine':cuisine}).encode("utf-8")
+    )
+        
+    try:
+        response_payload = json.loads(response["Payload"].read().decode("utf-8"))
+    except Exception as err:
+        return {
+            'statusCode': 400, 
+            'body': 'Bad request when retrieving messgaes from ElasticSearch',
+            'err': str(err)
+        }
+    
+    restaurant = response_payload
+
+    boto3.setup_default_session(region_name='us-east-1')
+    sns = boto3.client('sns')
+
+    text = '''Hello! Here are my {} restaurant suggestions for {} people, for {} at {}: {}, located at {}. Enjoy your meal!'''.format(
+        order['cuisine'].lower(), order['numberOfPeople'], order['diningDate'], 
         order['diningTime'], restaurant['name']['S'], restaurant['address']['S']
         )
     
