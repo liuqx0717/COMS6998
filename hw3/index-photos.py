74aea04ea0c08417c8b2f3a21ea049c7ee46186a
maruibin <maruibin123@gmail.com>
2019-12-06 11:53:12 -0500

    fix index-photos.py

--- hw3/index-photos.py
+++ hw3/index-photos.py
@@ -1,3 +1,4 @@
+import json
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import requests
 from requests_aws4auth import AWS4Auth
@@ -6,45 +7,58 @@ import json
 import uuid
 import datetime
 
-host = 'vpc-photos-rxqmfbkp3mzqjkteqaecvpkmtm.us-east-1.es.amazonaws.com'
-region = 'us-east-1'
-service = 'es'
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+def detect_labels(photo, bucket):
+    print("Connecting to rek...")
+    client = boto3.client('rekognition')
+    response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+                                    MaxLabels=10, MinConfidence=80)
+    print("rek response = ", json.dumps(response, indent=2))
+    label_list = [label['Name'].lower() for label in response['Labels']]
+    return label_list
 
 
-es = Elasticsearch(
-    hosts=[{'host': host, 'port': 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=RequestsHttpConnection
-)
+def lambda_handler(event, context):
+    print("event = ", json.dumps(event, indent=2))
+    records = event["Records"]
+    for record in records:
+        s3 = record["s3"]
+        photo = s3["object"]["key"]
+        bucket = s3["bucket"]["name"]
+    label_list = detect_labels(photo, bucket)
+    print("Labels detected: ", str(label_list))
 
-event = {'Records': [
-    {'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-east-1', 'eventTime': '2019-12-05T21:21:53.818Z',
-     'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'A3TRS0B5U108UU'},
-     'requestParameters': {'sourceIPAddress': '160.39.165.13'},
-     'responseElements': {'x-amz-request-id': '09A84B8D2C4090B3',
-                          'x-amz-id-2': 'JPas9nv/JMXpNJ1JhBDMkXsyKz6V0BZWd0wHS6khcTGXUQp6uJ2ujf0Ob6jUcWlopL7eo+Lhc5A='},
-     's3': {'s3SchemaVersion': '1.0', 'configurationId': 'PutPhoto',
-            'bucket': {'name': 'photobucket0', 'ownerIdentity': {'principalId': 'A3TRS0B5U108UU'},
-                       'arn': 'arn:aws:s3:::photobucket0'},
-            'object': {'key': 'Untitled-1.jpg', 'size': 272852, 'eTag': '811f924324c5ecb51779fa6fd3d8550f',
-                       'sequencer': '005DE974F1BB100A8B'}}}]}
+    # es config
+    host = 'vpc-photos-rxqmfbkp3mzqjkteqaecvpkmtm.us-east-1.es.amazonaws.com'
+    region = 'us-east-1'
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-print("timestamp = ", datetime.datetime.now().timestamp())
+    es = Elasticsearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    # print("\nes_info = ", es.info())
 
-document = {
-    "objectKey": "my-photo.jpg",
-    "bucket": "my-photo-bucket",
-    "createdTimestamp": "2018-11-05T12:40:02",
-    "labels": [
-        "person",
-        "dog",
-        "ball",
-        "park"
-    ]
-}
+    # index
+    document = {
+        "objectKey": photo,
+        "bucket": bucket,
+        "createdTimestamp": str(datetime.datetime.now()),
+        "labels": label_list
+    }
+    print("document = ", json.dumps(document, indent=2))
+    es.index(index="photos", doc_type="_doc", id=uuid.uuid4(), body=document)
 
-es.index(index="photos", doc_type="_doc", id=uuid.uuid4(), body=document)
+    # res = es.search(index="photos", q="dog")
+    # print("\nres = ", res, "\n")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
