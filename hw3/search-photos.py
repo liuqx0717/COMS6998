565e096bcd717c627322a9251133ed715af01965
maruibin <maruibin123@gmail.com>
2019-12-12 21:45:32 -0500

    fix search-photos

--- hw3/search-photos.py
+++ hw3/search-photos.py
@@ -22,29 +22,6 @@ def make_response(code, body):
     }
 
 
-def lex(message):
-    lf = boto3.client('lambda')
-    response = lf.invoke(
-        FunctionName='arn:aws:lambda:us-east-1:831292248611:function:photo_album_lex_lf',
-        Payload=json.dumps({'message': message}).encode("utf-8")
-    )
-    response_payload = json.loads(response["Payload"].read().decode("utf-8"))
-
-    if response_payload['status'] is False:
-        return {
-            'status': False,
-            'result': ''
-        }
-
-    keywords = response_payload['keywords']  # type list
-    res = search(keywords)
-
-    return {
-        'status': True,
-        'result': res
-    }
-
-
 def search(keywords):
     host = 'vpc-photos-rxqmfbkp3mzqjkteqaecvpkmtm.us-east-1.es.amazonaws.com'
     region = 'us-east-1'
@@ -61,18 +38,50 @@ def search(keywords):
     )
 
     # TODO: input format of labels. 'keywords' is a list of keywords
+    string = ""
+    for keyword in keywords:
+        string += str(keyword + " ")
+
+    print("string = ", string)
+
     query_body = {
         "query": {
-            "match": {"labels": str(keywords)}
+            "match": {"labels": string}
         }
     }
 
-    response = es.search(index="photos", body=query_body)
-    print(type(response))
+    response = es.search(index="photos", body=query_body)  # type: dict
     print(response)
     # TODO: search result processing
 
     return response
+
+
+def lex_search(message):
+    lf = boto3.client('lambda')
+    response = lf.invoke(
+        FunctionName='arn:aws:lambda:us-east-1:831292248611:function:photo_album_lex_lf',
+        Payload=json.dumps({'message': message}).encode("utf-8")
+    )
+    # print("response = ", json.dumps(json.loads(str(response)), indent=4))
+    print("type = ", type(response), "\nlex_response = ", response)
+    response_payload = json.loads(response["Payload"].read().decode("utf-8"))
+
+    if response_payload['status'] is False:
+        res = {
+            "status": False
+        }
+        return res
+
+    else:
+        keywords = response_payload['keywords']  # type: list
+        print("keywords = ", keywords)
+        result = search(keywords)
+        res = {
+            "status": True,
+            "result": result
+        }
+        return res
 
 
 def lambda_handler(event, context):
@@ -82,22 +91,48 @@ def lambda_handler(event, context):
         if event["resource"] == "/search":
             if event["httpMethod"] == "GET":
                 query = event['queryStringParameters']['q']
-                lex_res = lex(query)
-                print(lex_res)
-                if not lex_res['status']:
+                print("query = ", query)
+                lex_search_res = lex_search(query)
+                print("lex_search_res = ", json.dumps(lex_search_res, indent=4))
+                if not lex_search_res['status']:
+                    print("\nLex doesn't understand.")
                     return make_response(200, "Lex doesn't understand your words.")
+                else:
+                    if lex_search_res['result']['hits']['total']['value'] == 0:
+                        print("\nNo matches.")
+                        return make_response(200, "No matches.")
+                    else:
+                        results = []
+                        for hit in lex_search_res['result']['hits']['hits']:
+                            url = str("https://" + hit['_source']['bucket'] + ".s3.amazonaws.com/" + hit['_source'][
+                                'objectKey'])
+                            result = {"url": url}
+                            results.append(result)
+                        body = {"results": results}
+                        body = json.dumps(body)
+                        # print(json.dumps(body, indent=4))
+                        print("body = ", body)
+                        return {
+                            "statusCode": 200,
+                            "body": body,
+                            "headers": headers
+                        }
 
-
-                # This is a test return.
-                return {
-                    "statusCode": 200,
-                    "body": '{"results":[ {"url":"https://face-detect-6998.s3.amazonaws.com/10744586.jpg","labels":['
-                            '"labels","labels"]}, {"url":"https://face-detect-6998.s3.amazonaws.com/73596321.jpg",'
-                            '"labels":["labels","labels"]}, '
-                            '{"url":"https://face-detect-6998.s3.amazonaws.com/99778582.jpg","labels":["labels",'
-                            '"labels"]} ]}',
-                    "headers": headers
-                }
+                # # This is a test return.
+                # # body = '{"results":[ \
+                # # {"url":"https://face-detect-6998.s3.amazonaws.com/10744586.jpg","labels":["labels","labels"]}, \
+                # # {"url":"https://face-detect-6998.s3.amazonaws.com/73596321.jpg","labels":["labels","labels"]}, \
+                # # {"url":"https://face-detect-6998.s3.amazonaws.com/99778582.jpg","labels":["labels","labels"]}]}'
+                # body = '{"results":[ \
+                # {"url":"https://face-detect-6998.s3.amazonaws.com/10744586.jpg"}, \
+                # {"url":"https://face-detect-6998.s3.amazonaws.com/73596321.jpg"}, \
+                # {"url":"https://face-detect-6998.s3.amazonaws.com/99778582.jpg"}]}'
+                # print("body = ", json.dumps(json.loads(body), indent=4))
+                # return {
+                #     "statusCode": 200,
+                #     "body": body,
+                #     "headers": headers
+                # }
 
         # if resource/method not listed above
         return make_response(400, "Bad request.")
@@ -105,3 +140,108 @@ def lambda_handler(event, context):
     except Exception as err:
         print(err)
         return make_response(500, "Internal error - hw3.search-photos.py")
+
+# non-hit result:
+#  "result": {
+#         "took": 6,
+#         "timed_out": false,
+#         "_shards": {
+#             "total": 5,
+#             "successful": 5,
+#             "skipped": 0,
+#             "failed": 0
+#         },
+#         "hits": {
+#             "total": {
+#                 "value": 0,
+#                 "relation": "eq"
+#             },
+#             "max_score": null,
+#             "hits": []
+#         }
+#     }
+
+
+# Hit result:
+# "result": {
+#     "took": 4,
+#     "timed_out": false,
+#     "_shards": {
+#         "total": 5,
+#         "successful": 5,
+#         "skipped": 0,
+#         "failed": 0
+#     },
+#     "hits": {
+#         "total": {
+#             "value": 3,
+#             "relation": "eq"
+#         },
+#         "max_score": 1.2199391,
+#         "hits": [
+#             {
+#                 "_index": "photos",
+#                 "_type": "_doc",
+#                 "_id": "02c64477-bd55-43f1-88d3-b6c113904f77",
+#                 "_score": 1.2199391,
+#                 "_source": {
+#                     "objectKey": "plant-based-milk.jpg",
+#                     "bucket": "photobucket0",
+#                     "createdTimestamp": "2019-12-07 00:26:03.702685",
+#                     "labels": [
+#                         "beverage",
+#                         "drink",
+#                         "milk",
+#                         "plant",
+#                         "vegetable",
+#                         "nut",
+#                         "food",
+#                         "almond"
+#                     ]
+#                 }
+#             },
+#             {
+#                 "_index": "photos",
+#                 "_type": "_doc",
+#                 "_id": "3adc60e3-7291-49fc-955b-14bbd7aa5bac",
+#                 "_score": 1.0378369,
+#                 "_source": {
+#                     "objectKey": "plant-based-milk.jpg",
+#                     "bucket": "photobucket0",
+#                     "createdTimestamp": "2019-12-07 00:28:41.387028",
+#                     "labels": [
+#                         "drink",
+#                         "milk",
+#                         "beverage",
+#                         "plant",
+#                         "food",
+#                         "nut",
+#                         "vegetable",
+#                         "almond"
+#                     ]
+#                 }
+#             },
+#             {
+#                 "_index": "photos",
+#                 "_type": "_doc",
+#                 "_id": "9b9b6509-bdbd-4f9f-913c-6a6ccbd5d39c",
+#                 "_score": 0.86312973,
+#                 "_source": {
+#                     "objectKey": "plant-based-milk.jpg",
+#                     "bucket": "photobucket0",
+#                     "createdTimestamp": "2019-12-12 18:56:49.000993",
+#                     "labels": [
+#                         "milk",
+#                         "drink",
+#                         "beverage",
+#                         "plant",
+#                         "vegetable",
+#                         "food",
+#                         "nut",
+#                         "almond"
+#                     ]
+#                 }
+#             }
+#         ]
+#     }
+# }
