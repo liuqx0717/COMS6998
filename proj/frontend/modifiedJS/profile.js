2e681f87cb49b5730770f8724dcabb3bca829252
liyang <sjtuliyang@foxmail.com>
2019-12-16 18:40:22 -0500

    add link to items

--- proj/frontend/modifiedJS/profile.js
+++ proj/frontend/modifiedJS/profile.js
@@ -1,9 +1,8 @@
 var baseUrl = "https://api.moreforless.liuqx.net/v1";
 
 // find token from session storage. This token will expire every 60 mins
-// var token = "eyJraWQiOiJJT2VcL3p1VXU1SlFaQklkTHlxdmdhdHk2VmpCZjlWT2ZXaWFva0R4dWsyRT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyOTQ4MGI3NC0zNzg3LTRjNjQtYmMxYi03ZWZiODEyOWFhNDciLCJldmVudF9pZCI6ImZjMDkyZDZhLTZiOGItNGFkYi04ZjFkLTJjNjIwYmE3NDAxNyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE1NzY1MzIzMzAsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0RITzQ4MXNIayIsImV4cCI6MTU3NjUzNTkzMCwiaWF0IjoxNTc2NTMyMzMwLCJ2ZXJzaW9uIjoyLCJqdGkiOiI2ZmMzZjkwNi02OWMwLTRjZTktYWU4MS00OTgwMGJlZjEwMzciLCJjbGllbnRfaWQiOiIyZWk3ZGh2cmpocWc3MWt2dHF2ajh1bXZkYSIsInVzZXJuYW1lIjoiMjk0ODBiNzQtMzc4Ny00YzY0LWJjMWItN2VmYjgxMjlhYTQ3In0.KybOqJZNCfQVAdGcwdHAtONn2TLkT70Xvpwy7Bm1QtZlyRJNOQYWZrSXz36zD03noTcxSe3paGTP7gYuUNz8PpI4bJHL3RrOb6zyiGCM7AEhEoschZmbsZTxTfd1kANLfp29YoL4pRMaIRu6I_h7HTKgbYCIgrRmVZMJ1rJjlHFm3uPQk69Abm6mc5dNpfnA3nu9RRCeu9it0KeCUrIaIvIYjFE6AJIqcRPToOQFMi8IscRqrdaWpOmb4ZqZjPcfgv_wrTPW20gbuBNylkOZa9-iKiodGN9R2JnIvzb7NrhJmA_2P40JLuNT_wGFDgtYHTFRvyW176hHgd0Rw-YYLg";
-// var token = "eyJraWQiOiJJT2VcL3p1VXU1SlFaQklkTHlxdmdhdHk2VmpCZjlWT2ZXaWFva0R4dWsyRT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzNjI3YjNlMS04MDg1LTQ3MDMtYjdhNC1kYzFlY2EzODczZWYiLCJldmVudF9pZCI6IjUyNGM4OTU2LWQzMzEtNDIyOS1iMWJkLWVlMjYyNWIzOGM1OCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE1NzY1MzMyODcsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0RITzQ4MXNIayIsImV4cCI6MTU3NjUzNjg4NywiaWF0IjoxNTc2NTMzMjg3LCJ2ZXJzaW9uIjoyLCJqdGkiOiJiODliNjQ2OC0xOTA2LTQzODAtYTAxNi1lZTAxNGRjNDU5MDUiLCJjbGllbnRfaWQiOiIyZWk3ZGh2cmpocWc3MWt2dHF2ajh1bXZkYSIsInVzZXJuYW1lIjoiMzYyN2IzZTEtODA4NS00NzAzLWI3YTQtZGMxZWNhMzg3M2VmIn0.ntHvVE5-BDVVMPO3izIOnut7dzGGd9sQnmmQeyKoDAC0CNw3UdmCeMi1VSFFy7LkSVZh9u1KuXXsBq8_GJIR9O8Os2O74bQDf3XxkwlgmB-FdygWZb7v_GFRxnAL1oD33AH0SFboFAb1NjzVG2TrDFHkurFlFXVIQfOd2PVCA-SXi_BT1P4tICMnOOqfh6ihFhXl0Pp2UqJswFZAIw9fJuRWshYlNSwqgpNCKQTLjiYZVEn_X93hJCWHXZip21DkJj8AzMxn_R8meME9PKTyJ8SjmAT_mtlHg_AN2VFEcy-iGs6SfQ_5YCWlg57cpoqLbbDTLvqW4VWkUjpxKQEz2w";
-var token = "eyJraWQiOiJJT2VcL3p1VXU1SlFaQklkTHlxdmdhdHk2VmpCZjlWT2ZXaWFva0R4dWsyRT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyOTQ4MGI3NC0zNzg3LTRjNjQtYmMxYi03ZWZiODEyOWFhNDciLCJldmVudF9pZCI6Ijk5ZGJmOWI4LWUwMzEtNGFhZS1hNjI1LWZhMmI3ZTM3ZDU5MSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE1NzY1MzYwMzgsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0RITzQ4MXNIayIsImV4cCI6MTU3NjUzOTYzOCwiaWF0IjoxNTc2NTM2MDM4LCJ2ZXJzaW9uIjoyLCJqdGkiOiJmYzk1YTM4YS03YWUzLTRmOGQtOGNlZi00MWZjOTk2OTJjMWIiLCJjbGllbnRfaWQiOiIyZWk3ZGh2cmpocWc3MWt2dHF2ajh1bXZkYSIsInVzZXJuYW1lIjoiMjk0ODBiNzQtMzc4Ny00YzY0LWJjMWItN2VmYjgxMjlhYTQ3In0.TgT0RJvtiZcEdgyF86ezbYo5vD-s_Ay1vHGIX0L37F1oTtJ1zCvamv9JKF7lbSOUc3rq0zdYN71i92-Fk7hxeYitUOYPT0hWS4hoUBLYxIwSlV_JPEOpwbCClOLuiQE-edkRqMR4EXtjne4Ctk2Uwf6187VCFPstCWOyxqVVgSCInScgFEWm_Iuaf2pgQa3SNzVwtBpnNsJRRfATz-uSqMP6csQddU1885ExEtu1DSnoGsRPSZzBSHqG0ruV4e3VcyoX2dP1Stu7XyHW4ZxRwZi7HJlXCZjlUCnq5YP-deCn_w7JcjG1MhZRgkm4nqt4fELV90PyHaO-BZoQLueqVw";
+var token = "eyJraWQiOiJJT2VcL3p1VXU1SlFaQklkTHlxdmdhdHk2VmpCZjlWT2ZXaWFva0R4dWsyRT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzYzFkNzkyMS1lYmZkLTRmMjItYTlkMS1iNmM3Mjc1NWY4MWIiLCJldmVudF9pZCI6ImNlOTExZDdmLWViNTItNDRjYS04NDRkLTI3NDkwM2Y4YjhkZSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE1NzY1MzkxMTksImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX0RITzQ4MXNIayIsImV4cCI6MTU3NjU0MjcxOSwiaWF0IjoxNTc2NTM5MTE5LCJ2ZXJzaW9uIjoyLCJqdGkiOiI0YTc0NDk3Ny1kNDM5LTQ1Y2MtOTA1Yy1lNzVkNWY5MWNlZGMiLCJjbGllbnRfaWQiOiIyZWk3ZGh2cmpocWc3MWt2dHF2ajh1bXZkYSIsInVzZXJuYW1lIjoiM2MxZDc5MjEtZWJmZC00ZjIyLWE5ZDEtYjZjNzI3NTVmODFiIn0.qKC9vS5dHLFejcGPefwSHy7110ilzcxIxjaOKGp1fVtqElHu0XzTQpkFG-WZ2zPFglqGuioCOMbkddlhN4NeFnI0B1ZUwVR2L31cUyQxn2gbeLwUbg9j6XEHD5p_Y4OTZnc6jU-J_cL5letNd5sQB3KO4--opdSvnMJKclZiEOAB8Fd_IPcyNyOW0jClfyqMm3eLd5eIkSc1Y3s_Si0LhO-_HOMYGnvwu9NyOL4R-4jhpIFR647G7LGYJdXi_vZBhCbKvdKJsVUK9A3yUcZnVO0qYhst2Ef9gUw0Coe2FjJVBH2xQiziyQwICV2t1UENiWDrKP1jiyaX3o4XqAUJCA";
+//var token = "eyJraWQiOiJJT2VcL3p1VXU1SlFaQklkTHlxdmdhdHk2VmpCZjlWT2ZXaWFva0R4dWsyRT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyOTQ4MGI3NC0zNzg3LTRjNjQtYmMxYi03ZWZiODEyOWFhNDciLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNTc2NTM3ODEwLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9ESE80ODFzSGsiLCJleHAiOjE1NzY1NDE0MTAsImlhdCI6MTU3NjUzNzgxMCwidmVyc2lvbiI6MiwianRpIjoiNWIzYTc1ODctMTkzMi00NjVlLWE1MTYtZDVlMjRjZTM5YTM3IiwiY2xpZW50X2lkIjoiMmVpN2RodnJqaHFnNzFrdnRxdmo4dW12ZGEiLCJ1c2VybmFtZSI6IjI5NDgwYjc0LTM3ODctNGM2NC1iYzFiLTdlZmI4MTI5YWE0NyJ9.Y0yGvKfDIb13H3d_3BStlq4cVuVhBaprXRZFmaVqUyutpktQh9dvpmZ59tjmKGQLTcJpLCVxfpJbjHJtGcI40A4_B1hMLiVgP9M2wyQnCsqaS-qm9HUov_6pNR0YUWlZq3-Jx0dJum7IOadWujRX9U2oBmf_iSuc-5h3EiOR5k44VRrOGgYneucLg5Tj51168Psn8MtwJCg3PXcRBQI9E-1TXNqCwDn63QFa6EJJOpqku6hY_xnwuKgTylf6648IkgYX6lqVxorIv3AMhg8QlvVxe8XEhlCylEE6DOzgnyK7oW2IQqwd5B_IqEyut4P8WbI8v5nfIpaMBFq6mV1_7g";
 // var token = sessionStorage.getItem('access-token');
 $(document).ready(function() {
     // call for /myhome GET API to get user info
@@ -105,13 +104,13 @@ function update_user(event){
     });
 }
 
-function generate_item_html(item_imgurl, item_title, item_price, item_finish_time, item_seller_name, item_seller_address, order_status, order_id) {
+function generate_item_html(item_imgurl, item_id, item_title, item_price, item_finish_time, item_seller_name, item_seller_address, order_status, order_id) {
     item_template = `<div class="row">
                                 <div class="col-3">
                                     <img class="img-fluid" src="{item_imgurl}" alt="">
                                 </div>
                                 <div class="col-lg-9">
-                                    <a href="#">Title:{item_title}</a>
+                                    <a href="/single-product.html?item_id={item_id}">Title:{item_title}</a>
                                     <br>
                                     Price:{item_price}
                                     <br>
@@ -130,7 +129,8 @@ function generate_item_html(item_imgurl, item_title, item_price, item_finish_tim
     item_template = item_template.replace("{item_imgurl}",item_imgurl).replace("{item_title}",item_title)
         .replace("{item_price}",item_price).replace("{item_finish_time}",item_finish_time)
         .replace("{order_status}",order_status).replace("{order_id}",order_id)
-        .replace("{item_seller_name}", item_seller_name).replace("{item_seller_address}", item_seller_address);
+        .replace("{item_seller_name}", item_seller_name).replace("{item_seller_address}", item_seller_address)
+        .replace("{item_id}", item_id);
     return item_template
 }
 
@@ -170,7 +170,7 @@ function show_orders(response) {
                     alert("/items/ GET function" + errMsg);
                 }
             });
-            var item_content = generate_item_html(item_imgurl, item_title, item_price, item_finish_time, item_seller_name, item_seller_address, order_status, order_id);
+            var item_content = generate_item_html(item_imgurl, item_id, item_title, item_price, item_finish_time, item_seller_name, item_seller_address, order_status, order_id);
             $("#profile_item_list").prepend(item_content);
         }
     }
