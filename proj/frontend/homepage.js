3ffab23961f02484d884d297a3e990a19040d711
liuqx0717 <857540869@qq.com>
2019-12-16 15:57:19 -0500

    Added getQueryVariable to homepage.js

--- proj/frontend/homepage.js
+++ proj/frontend/homepage.js
@@ -92,3 +92,17 @@ function addRecommItem(title, imgUrl, link, price, prevPrice = 0){
 
     $("#recomm").append(htmlStr);
 }
+
+
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (decodeURIComponent(pair[0]) == variable) {
+          return decodeURIComponent(pair[1]);
+      }
+  }
+  console.log('Query variable %s not found', variable);
+}
